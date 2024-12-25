f3af520e06c0bb63219cded9d4e97a623973a41c
irfandhamhudi <irfandhamhudi712@gmail.com>
2024-12-26 00:45:06 +0700

    problem solve

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/database.js";
+import connectDB from "../config/database.js";
 import cookieParser from "cookie-parser";
 
 // Import routes
-import userRoutes from "./route/userRoute.js";
-import avatarRoutes from "./route/avatarRoute.js";
-import productRoutes from "./route/productRoute.js";
+import userRoutes from "../route/userRoute.js";
+import avatarRoutes from "../route/avatarRoute.js";
+import productRoutes from "../route/productRoute.js";
 
-const app = express();
 dotenv.config();
-
 connectDB();
 
+const app = express();
 app.use(cookieParser());
 
 // Middleware untuk parsing body JSON
@@ -23,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:3000", // Untuk emulator Android
   "http://10.0.2.2:3000",
-  "http:// 192.168.1.4:3000",
+  "http://192.168.1.4:3000",
   "http://127.0.0.1:3000",
   "*",
 ];
@@ -49,13 +48,11 @@ app.use("/api/avatar", avatarRoutes);
 // Route for products
 app.use("/api/product", productRoutes);
 
-const PORT = process.env.PORT || 5000;
-// const HOST = "192.168.1.5"; // Alamat IP lokal Anda
-
-// app.listen(PORT, HOST, () => {
-//   console.log(`Server running on http://${HOST}:${PORT}`);
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
+
+// Menjalankan express sebagai fungsi serverless untuk Vercel
+export default (req, res) => {
+  app(req, res);
+};
