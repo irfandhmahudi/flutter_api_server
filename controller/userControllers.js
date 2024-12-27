b266d6c2ba81f5090c0df6e3544accf6b26d822c
irfandhamhudi <irfandhamhudi712@gmail.com>
2024-12-27 22:04:28 +0700

    problem solve

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -147,7 +147,7 @@ export const loginUser = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Login successful",
-      // token,
+      token,
 
       // user,
     });
