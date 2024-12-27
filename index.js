bdc9e97020199333ff543cc5607f282b22555abb
irfandhamhudi <irfandhamhudi712@gmail.com>
2024-12-28 00:29:55 +0700

    problem solve

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "https://flutter-ousejbj8o-irfandhamhudis-projects.vercel.app", // URL frontend Anda
-    credentials: true, // Ini memungkinkan cookies dikirim bersama permintaan
+    credentials: include, // Ini memungkinkan cookies dikirim bersama permintaan
   })
 );
 
