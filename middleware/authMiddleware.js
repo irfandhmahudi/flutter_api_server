50d6e005a2741a181e740ddff76239b879de3cef
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-01-05 19:34:21 +0700

    commit solve

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const authMiddleware = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const user = await User.findById(decoded._id);
+    const user = await User.findById(decoded.id);
 
     if (!user) {
       return res.status(401).json({ success: false, message: "Unauthorized" });
