249457a84e999c25358541697a4ff4b4f300e2c2
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-01-09 01:46:28 +0700

    commit solve

--- controller/productControllers.js
+++ controller/productControllers.js
@@ -158,14 +158,14 @@ export const getProductById = async (req, res) => {
     // }).format(priceAfterDiscount);
 
     // // Mengembalikan objek produk dengan harga yang telah diformat
-    // res.status(200).json({
-    //   success: true,
-    //   data: {
-    //     ...product.toObject(),
-    //     price: formattedPrice,
-    //     priceAfterDiscount: formattedPriceAfterDiscount,
-    //   },
-    // });
+    res.status(200).json({
+      success: true,
+      data: {
+        ...product.toObject(),
+        price: formattedPrice,
+        priceAfterDiscount: formattedPriceAfterDiscount,
+      },
+    });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
