60374fb3f6daad7cf451e05c22279e5dc5f280f0
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-01-08 10:21:15 +0700

    commit solve

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
       "http://10.0.2.2:3000",
       "http://10.0.2.16:3000",
       "http://192.168.1.2:3000",
-      "http:localhost:5173",
+      "http://localhost:5173/",
     ],
     credentials: true, // Membolehkan cookies dikirimkan
   })
