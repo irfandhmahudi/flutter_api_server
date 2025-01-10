61ac55bda11ce01cba0cb4c776dd90ed76acfee8
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-01-10 12:33:44 +0700

    commit solve

--- route/coupunRoutes.js
+++ route/coupunRoutes.js
@@ -5,13 +5,14 @@ import {
   deleteCoupon,
   applyCoupon,
 } from "../controller/coupunController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Routes
-router.get("/", getCoupons); // GET all coupons
-router.post("/", createCoupon); // POST create a new coupon
-router.delete("/:id", deleteCoupon); // DELETE a coupon by ID
-router.post("/apply", applyCoupon); // POST apply a coupon
+router.get("/", authMiddleware, getCoupons); // GET all coupons
+router.post("/", authMiddleware, createCoupon); // POST create a new coupon
+router.delete("/:id", authMiddleware, deleteCoupon); // DELETE a coupon by ID
+router.post("/apply", authMiddleware, applyCoupon); // POST apply a coupon
 
 export default router;
